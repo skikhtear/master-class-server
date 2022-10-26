928b5eab7d9282a0b0b6447f87b25f76ce045847
skikhtear <skikhtear@gmail.com>
2022-10-27 03:21:59 +0600

    server

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
+const courses = require('./data/courses.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Master class api running')
+});
